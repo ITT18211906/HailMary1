0dda12845701b86237e37d9f9bf0d1730430d1e5
Efrain Astorga <efrain.astorga18@tectijuana.edu.mx>
2020-11-27 14:41:16 -0800

    Edit | Details | Delete

--- HailMary1/Controllers/CarController.cs
+++ HailMary1/Controllers/CarController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace HailMary1.Controllers
 {
@@ -16,10 +17,55 @@ namespace HailMary1.Controllers
             var cars = db.Cars.ToList();
             return View(cars);
         }
+        public ActionResult Edit(int id)
+        {
+            var car = db.Cars.Find(id);
+            return View(car);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Car c)
+        {
+            var car = db.Cars.Find(id);
+            try
+            {
+                car.Nombre = c.Nombre;
+                car.Descripcion = c.Descripcion;
+                car.Precio = c.Precio;
+                //db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(car);
+            }
+        }
+        public ActionResult Details(int id)
+        {
+            var cars = db.Cars.Find(id);
+            return View(cars);
+        }
         public ActionResult Delete(int id)
         {
             var cars = db.Cars.Find(id);
             return View(cars);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Car c)
+        {
+            var car = db.Cars.Find(id);
+            try
+            {
+                db.Cars.Remove(car);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(car);
+            }
         }
     }
 }
